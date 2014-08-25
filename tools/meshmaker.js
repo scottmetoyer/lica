3f8bce2b71e49d400a3566a0051295c833810553
Scott Metoyer <scott.metoyer@gmail.com>
2014-08-24 20:55:29 -0700

    Implemented first pass on nodes

--- tools/meshmaker.js
+++ tools/meshmaker.js
@@ -2,7 +2,8 @@ var Meshmaker = (function () {
     var canvas = document.getElementById('canvas');
     var context = canvas.getContext('2d');
     var polygons = [];
-    var draggingPoint = null;
+    var nodes = [];
+    var draggingVertex = null;
     var currentPolygon = null;
     var image = null;
 
@@ -30,8 +31,9 @@ var Meshmaker = (function () {
                     canvas.width = image.width;
                     context.drawImage(image, 0, 0);
                     polygons = [];
-                    draggingPoint = null;
+                    draggingVertex = null;
                     currentPolygon = null;
+                    currentLink = null;
                 }
             };
         })(file);
@@ -49,13 +51,17 @@ var Meshmaker = (function () {
         var bottomRight = { X: position.X + size, Y: position.Y + size };
         var bottomLeft = { X: position.X - size, Y: position.Y + size };
 
-        var polygon = { vertices: [], centroid: {X:0, Y: 0}};
+        var polygon = { vertices: [], centroid: { X: 0, Y: 0 } };
         polygon.vertices.push(topLeft);
         polygon.vertices.push(topRight);
         polygon.vertices.push(bottomRight);
         polygon.vertices.push(bottomLeft);
-        polygon.centroid = Polygon.computeCentroid(polygon.vertices, 4);
         polygons.push(polygon);
+    }
+
+    function dropNode(position) {
+        var node = { X: position.X, Y: position.Y };
+        nodes.push(node);
     }
 
     function drawCircle(position, radius, color) {
@@ -67,18 +73,13 @@ var Meshmaker = (function () {
 
     function drawPolygon(polygon) {
         if (polygon.vertices.length > 0) {
-            var drawCentroid = false;
-
             if (Polygon.isConcave(polygon.vertices)) {
                 context.fillStyle = 'rgba(255, 0, 0, 0.5)';
             }
             else {
-                drawCentroid = true;
                 context.fillStyle = 'rgba(0, 0, 255, 0.5)';
             }
             context.beginPath();
-
-            // Set start point
             context.moveTo(polygon.vertices[0].X, polygon.vertices[0].Y);
 
             // Draw the polygon
@@ -95,11 +96,6 @@ var Meshmaker = (function () {
             for (var i = 0; i < polygon.vertices.length; i++) {
                 drawCircle({ X: polygon.vertices[i].X, Y: polygon.vertices[i].Y }, 5, 'yellow');
             }
-
-            // Compute and draw the centroid
-            if (drawCentroid) {
-                drawCircle({ X: polygon.centroid.X, Y: polygon.centroid.Y }, 5, 'white');
-            }
         }
     }
 
@@ -115,48 +111,60 @@ var Meshmaker = (function () {
         };
     }
 
+    function checkHit(position, target) {
+        var hit = false;
+
+        if (position.X > target.X - 5 && position.X < target.X + 5 && position.Y > target.Y - 5 && position.Y < target.Y + 5) {
+            hit = true;
+        }
+
+        return hit;
+    }
+
     function handleMouseDown(event) {
         var position = getCursorPosition(event);
+        var areaClick = document.getElementById('areas').checked;
+        var nodeClick = document.getElementById('nodes').checked;
 
-        // Are we creating areas or linking polygons?
-        if (document.getElementById('areas').checked) {
-            // Check if over a drag handle
+        // Check if over a drag handle
+        if (areaClick) {
             for (var x = 0; x < polygons.length; x++) {
                 var polygon = polygons[x];
 
                 for (var y = 0; y < polygon.vertices.length; y++) {
-                    if (position.X > polygon.vertices[y].X - 5 && position.X < polygon.vertices[y].X + 5
-                        && position.Y > polygon.vertices[y].Y - 5 && position.Y < polygon.vertices[y].Y + 5) {
-                        draggingPoint = polygon.vertices[y];
+                    if (checkHit(position, polygon.vertices[y])) {
+                        draggingVertex = polygon.vertices[y];
                         currentPolygon = polygon;
                     }
                 }
             }
+        }
 
-            if (image && !draggingPoint) {
-                var position = getCursorPosition(event);
+        if (image && !draggingVertex) {
+            var position = getCursorPosition(event);
+
+            if (areaClick) {
                 dropPolygon(position);
             }
-        } else {
 
+            if (nodeClick) {
+                dropNode(position);
+            }
         }
     }
 
     function handleMouseUp(event) {
-        draggingPoint = null;
+        draggingVertex = null;
         currentPolygon = null;
     }
 
     function handleMouseMove(event) {
-        if (draggingPoint) {
-            var position = getCursorPosition(event);
+        var position = getCursorPosition(event);
 
+        if (draggingVertex) {
             // Check bounds
-            draggingPoint.X = position.X;
-            draggingPoint.Y = position.Y;
-
-            // Recalulate the centroid
-            currentPolygon.centroid = Polygon.computeCentroid(currentPolygon.vertices, 4);
+            draggingVertex.X = position.X;
+            draggingVertex.Y = position.Y;
         }
     }
 
@@ -170,6 +178,23 @@ var Meshmaker = (function () {
 
         for (var i = 0; i < polygons.length; i++) {
             drawPolygon(polygons[i]);
+        }
+
+        for (var i = 0; i < nodes.length; i++) {
+            drawCircle(nodes[i], 5, 'white');
+        }
+
+        // Draw the nodes and links
+        if (nodes.length > 0) {
+            context.beginPath();
+            context.moveTo(nodes[0].X, nodes[0].Y);
+
+            for (var i = 1; i < nodes.length; i++) {
+                context.lineTo(nodes[i].X, nodes[i].Y);
+            }
+
+            context.strokeStyle = 'white';
+            context.stroke();
         }
     }
 
