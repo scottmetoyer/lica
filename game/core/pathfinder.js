56fc9327a1ad67075b423d039fef6d49a98f1dae
Scott Metoyer <scott.metoyer@gmail.com>
2014-10-06 21:35:43 -0700

    Worked on A* implementation

--- game/core/pathfinder.js
+++ game/core/pathfinder.js
@@ -3,13 +3,37 @@ var Pathfinder = (function (parameters) {
     var openList = [];
     var mesh = parameters.mesh;
 
+    // http://www.policyalmanac.org/games/aStarTutorial.htm
     // Parameters:
     // start: The Vector2 start point
     // end: The Vector2 end point
     function navigate(start, end) {
-        while (openList.length > 0) {
+        openList = [];
+        var node = { point: start, parent: null };
+        openList.push(node);
+        var loopCount = 0;
 
+        while (openList.length > 0) {
+            var polygon = findPolygonByPoint(node);
+
+            // Add points (centroid and links) in this polygon to the open list
+            openList.push({ point: polygon.centroid, parent: node });
+
+            var links = findPointsInPolygon(polygon);
+
+            for (var i = 0; i < links.length; i++) {
+                openList.push({ point: links[i], parent: node });
+            }
+
+            // Remove the lowest scored node and put it on the closed list
         }
+    }
+
+    function findPolygonByPoint(point) {
+    }
+
+    function findPointsInPolygon(polygon) {
+
     }
 
     return {
