57cc4c20d142efba1ae4de16ab565ff8620109e4
Scott Metoyer <scott.metoyer@gmail.com>
2015-09-09 12:56:21 -0700

    Added interface open/close animations

--- game/scripts/game.js
+++ game/scripts/game.js
@@ -1,15 +1,46 @@
 window.onload = function() {
+  var interfaceVisible = false;
+  var interfaceHeight = 100;
+  var showInterface, hideInterface;
+
   var game = new Phaser.Game(800, 600, Phaser.AUTO, '', {
     preload: preload,
-    create: create
+    create: create,
+    update: update
   });
 
   function preload() {
     game.load.image('logo', './assets/backgrounds/phaser-logo-small.png');
+    game.load.image('interface', './assets/backgrounds/interface.png');
   }
 
   function create() {
+    game.input.mouse.capture = true;
+
     var logo = game.add.sprite(game.world.centerX, game.world.centerY, 'logo');
     logo.anchor.setTo(0.5, 0.5);
+
+    // Setup interface open animation
+    var interfaceSlider = game.add.sprite(0, interfaceHeight * -1, 'interface');
+    showInterface = game.add.tween(interfaceSlider).to({
+      y: 0
+    }, 500, Phaser.Easing.Exponential.In, false);
+    hideInterface = game.add.tween(interfaceSlider).to({
+      y: interfaceHeight * -1
+    }, 500, Phaser.Easing.Exponential.Out, false);
+  }
+
+  function update() {
+    var y = game.input.mousePointer.y;
+
+    if (y < 10 && interfaceVisible === false) {
+      interfaceVisible = true;
+      showInterface.start();
+    }
+
+    if (y > interfaceHeight && interfaceVisible === true) {
+      interfaceVisible = false;
+      hideInterface.start();
+    }
   }
 };
