d865535e8fdf46ae55b245739f238e9f8b18e255
Scott Metoyer <scott.metoyer@gmail.com>
2015-09-11 15:14:03 -0700

    Created interface buttons

--- game/scripts/game.js
+++ game/scripts/game.js
@@ -1,6 +1,7 @@
 window.onload = function() {
   var interfaceVisible = false;
   var interfaceHeight = 60;
+  var interfaceButtonGroup;
   var showInterface, hideInterface;
 
   // HVGA resolution to work better on wide devices and phones, 480 x 270, 16:9 aspect ratio
@@ -12,7 +13,16 @@ window.onload = function() {
 
   function preload() {
     game.load.image('logo', './assets/backgrounds/phaser-logo-small.png');
-    game.load.image('interface', './assets/backgrounds/interface.png');
+
+    // Load the interface icons
+    game.load.image('lookButton', './assets/interface/look_button.png');
+    game.load.image('walkButton', './assets/interface/walk_button.png');
+    game.load.image('talkButton', './assets/interface/talk_button.png');
+    game.load.image('helpButton', './assets/interface/help_button.png');
+    game.load.image('useButton', './assets/interface/use_button.png');
+    game.load.image('optionsButton', './assets/interface/options_button.png');
+    game.load.image('inventoryButton', './assets/interface/inventory_button.png');
+    game.load.image('itemButton', './assets/interface/item_button.png');
   }
 
   function create() {
@@ -21,12 +31,31 @@ window.onload = function() {
     var logo = game.add.sprite(game.world.centerX, game.world.centerY, 'logo');
     logo.anchor.setTo(0.5, 0.5);
 
-    // Setup interface open animation
-    var interfaceSlider = game.add.sprite(0, interfaceHeight * -1, 'interface');
-    showInterface = game.add.tween(interfaceSlider).to({
+    // Setup interface
+    interfaceButtonGroup = game.add.group();
+
+    var walkButton = game.make.button(0, 0, 'walkButton', null);
+    var lookButton = game.make.button(60, 0, 'lookButton', null);
+    var useButton = game.make.button(120, 0, 'useButton', null);
+    var talkButton = game.make.button(180, 0, 'talkButton', null);
+    var itemButton = game.make.button(240, 0, 'itemButton', null);
+    var inventoryButton = game.make.button(300, 0, 'inventoryButton', null);
+    var optionsButton = game.make.button(360, 0, 'optionsButton', null);
+    var helpButton = game.make.button(420, 0, 'helpButton', null);
+
+    interfaceButtonGroup.add(lookButton);
+    interfaceButtonGroup.add(useButton);
+    interfaceButtonGroup.add(walkButton);
+    interfaceButtonGroup.add(talkButton);
+    interfaceButtonGroup.add(helpButton);
+    interfaceButtonGroup.add(optionsButton);
+    interfaceButtonGroup.add(inventoryButton);
+    interfaceButtonGroup.add(itemButton);
+
+    showInterface = game.add.tween(interfaceButtonGroup).to({
       y: 0
     }, 500, Phaser.Easing.Exponential.In, false);
-    hideInterface = game.add.tween(interfaceSlider).to({
+    hideInterface = game.add.tween(interfaceButtonGroup).to({
       y: interfaceHeight * -1
     }, 500, Phaser.Easing.Exponential.Out, false);
   }
